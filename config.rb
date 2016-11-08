f3bf08265680df5251e0cd87fdbd62573076eb85
Andy Williams <andy@andywilliams.me>
2016-11-08 23:43:11 +0000

    Update to use the name Samwise to avoid confusion
    In the future hopefully we can pull it back as a gollum extension

--- config.rb
+++ config.rb
@@ -9,3 +9,15 @@ It is especially useful for customizing supported formats/markups. For more info
 =end
 
 # enter your Ruby code here ...
+
+Gollum::Page.send :remove_const, :FORMAT_NAMES if defined? Gollum::Page::FORMAT_NAMES
+
+# first remove all markups:
+Gollum::Markup.formats.clear
+
+# and then define the sole markup to be supported:
+Gollum::Markup.formats[:markdown] = {
+    :name => "Markdown",
+    :regexp => /md|mkdn?|mdown|markdown/
+}
+
